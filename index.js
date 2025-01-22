9d4063bd3ef68aee8d4991153d57319783afff48
Damokles <damokless@protonmail.com>
2025-01-22 15:16:42 +0100

    feat: get all data from swapi

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const { default: get_all_data } = require('./get/get_data');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+        server.route({
+        method: 'GET',
+        path: '/get-all-data',
+        handler: async () => {
+           return await get_all_data()
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
